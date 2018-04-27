156ab5455528d2fa82b59ddca0a06824a3a0254f
Pawel Wasil <wasil.paul@gmail.com>
2018-04-27 15:59:52 +0200

    PieChart added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import { render } from 'react-dom';
+import { PieChart, Pie, Tooltip }  from 'recharts'
 
-const Dashboard = () => <div> okoko </div>
+const data = [
+  {
+    value: 30,
+    name: 'biegi po lesie'
+  },
+  {
+    value: 20,
+    name: 'biegi po miescie'
+  },
+  {
+    value: 50,
+    name: 'biegi bez butow'
+  }
+
+]
+
+const Dashboard = () => (
+  <div >
+    <PieChart width={500} height={500}>
+      <Pie 
+        data={data}
+        dataKey="value"
+        nameKey="name"
+        fill="#000fff" 
+      />
+      <Tooltip />
+    </PieChart>
+  </div>
+);
 
 export default Dashboard
