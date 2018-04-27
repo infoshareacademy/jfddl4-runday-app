e31cb1e89fdfb749c2c10fa1fa831f7071f9398c
Pawel Wasil <wasil.paul@gmail.com>
2018-04-27 16:27:12 +0200

    color to PieChart added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,35 +1,43 @@
 import React from 'react'
 import { render } from 'react-dom';
-import { PieChart, Pie, Tooltip }  from 'recharts'
+import { PieChart, Pie, Tooltip, Cell } from 'recharts'
 
 const data = [
-  {
-    value: 30,
-    name: 'biegi po lesie'
-  },
-  {
-    value: 20,
-    name: 'biegi po miescie'
-  },
-  {
-    value: 50,
-    name: 'biegi bez butow'
-  }
-
+    {
+        value: 30,
+        name: 'biegi po lesie',
+        color: 'green'
+    },
+    {
+        value: 20,
+        name: 'biegi po miescie',
+        color: 'red'
+    },
+    {
+        value: 30,
+        name: 'biegi bez butow',
+        color: 'yellow'
+    }
 ]
 
+const pie_cells = data.map((entry, index) => {
+    return (<Cell key={index} fill={entry.color} />)
+})
+
 const Dashboard = () => (
-  <div >
-    <PieChart width={500} height={500}>
-      <Pie 
-        data={data}
-        dataKey="value"
-        nameKey="name"
-        fill="#000fff" 
-      />
-      <Tooltip />
-    </PieChart>
-  </div>
+    <div >
+        <h2>Running List </h2>
+        <PieChart width={500} height={500}>
+            <Pie
+                data={data}
+                dataKey="value"
+                nameKey="name"
+            >
+                {pie_cells}
+            </Pie>
+            <Tooltip />
+        </PieChart>
+    </div>
 );
 
 export default Dashboard
