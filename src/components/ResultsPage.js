e209be3a26edf93c3b6401056f86e906bb482b2e
Pawel Wasil <wasil.paul@gmail.com>
2018-05-11 16:48:16 +0200

    google maps and marker with lat and lng added

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,5 +1,53 @@
-import React from 'react'
 
-const ResultsPage = () => <div></div>
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-export default ResultsPage
+
+class SimpleMap extends Component {
+    state = {
+        center: {
+            lat: 51.246452,
+            lng: 22.568445
+        },
+        markers: [],
+        zoom: 11
+    };
+
+    placeMarker = ({ lat, lng }) => {
+        const markerData = { lat, lng }
+        this.setState({
+            markers: this.state.markers.concat(markerData),
+        })
+        console.log(this.state.markers)
+    }
+
+
+
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '50vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyBjbSX619TpTJBp9afQKJUuueKAF9ZGawc' }}
+                    defaultCenter={this.state.center}
+                    defaultZoom={this.state.zoom}
+                    onClick={this.placeMarker}
+                >
+                    {/* <div lat={this.state.center.lat} lng={this.state.center.lng}>
+                        tutaj
+                    </div> */}
+                    {this.state.markers.map((marker, i) => <Marker text={i+1} lat={marker.lat} lng={marker.lng}/>)}
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+const Marker = ({text}) => (
+    <div>
+        {text}
+    </div>
+)
+
+export default SimpleMap;
